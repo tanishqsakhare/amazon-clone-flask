2ec7f9c0de9f945e230be8dd4400b57dd0fe9a5c
Tanishq Sakhare <93567125+tanishqsakhare@users.noreply.github.com>
2025-07-04 14:21:56 +0530

    Add files via upload

---
+++ app/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length
+
+class RegisterForm(FlaskForm):
+    username = StringField("Username", validators=[InputRequired(), Length(min=4, max=20)])
+    password = PasswordField("Password", validators=[InputRequired(), Length(min=6, max=80)])
+    submit = SubmitField("Register")
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired()])
+    submit = SubmitField("Login")
